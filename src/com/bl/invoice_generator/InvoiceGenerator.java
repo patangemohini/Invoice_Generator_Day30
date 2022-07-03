1ccde0c492c5e0b318ef957f43e6f6ac3f8124c1
mohini patange <patangemohini5@gmail.com>
2022-07-03 12:14:14 +0530

    Added : Welcome message

---
+++ src/com/bl/invoice_generator/InvoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.bl.invoice_generator;
+
+public class InvoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("Invoice generator program!!!!");
+    }
+}
